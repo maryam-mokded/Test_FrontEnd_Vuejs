afbe6c58ebe5cad61c4a4801606c4d7391da1c6d
NourG8 <nourgerfali08@gmail.com>
2022-07-21 17:09:07 +0100

    Frist commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    icons: {
+        iconfont: 'md' || 'fa' 
+      },
+      theme: {
+        themes: {
+            light: {
+                background: colors.grey.lighten2, // Not automatically applied
+              },
+          dark: {
+            background: colors.shades.white, // If not using lighten/darken, use base to return hex
+          },
+        },
+      },
+});
